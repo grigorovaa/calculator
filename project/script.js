125242bad29b4869d3bdb41e94fc027adcc1149c
grigorovaa <megrimarik13@gmail.com>
2023-05-30 23:16:16 +0300

    added logic

--- project/script.js
+++ project/script.js
@@ -1,117 +1,214 @@
 const calculator = document.querySelector("#calculator");
-const numberOfFields = 20;
 
-function createButtons() {
-  for (let i = 0; i < numberOfFields; i++) {
-    let field = document.createElement("div");
-    field.classList.add("field");
-    field.id = `field-${i}`;
-    const values = [
-      "",
-      "AC",
-      "+/-",
-      "%",
-      "÷",
-      7,
-      8,
-      9,
-      "×",
-      4,
-      5,
-      6,
-      "-",
-      1,
-      2,
-      3,
-      "+",
-      0,
-      ",",
-      "=",
-    ];
-    field.append(values[i]);
-    calculator.appendChild(field);
-    field.onclick = checkValue;
-  }
+const displayField = document.querySelector("#display");
+displayField.textContent = "0";
+
+let values = [];
+
+const fieldFunctions = {
+  erase: eraseInput,
+  changeSign: changeSign,
+  // percent: countPercent,
+  seven: addDigits,
+  eight: addDigits,
+  nine: addDigits,
+  four: addDigits,
+  five: addDigits,
+  six: addDigits,
+  subtract: subtract,
+  one: addDigits,
+  two: addDigits,
+  three: addDigits,
+  add: add,
+  zero: addDigits,
+  floatingNum: addPoint,
+  equals: countValue,
+};
+
+function findButtons() {
+  let fields = document.querySelectorAll(".field");
+  fields.forEach((field) => {
+    const fieldFunction = fieldFunctions[field.id];
+
+    // field.addEventListener("keydown", highlightField);
+    // field.addEventListener("keyup", unhighlightField); //doesnt work
+
+    if (fieldFunction) {
+      field.addEventListener("click", checkValue);
+    }
+  });
 }
 
-// function someFunc() {
-//   console.log("hey");
-// }
+function listenForNumber() {
+  document.addEventListener("keydown", function (event) {
+    const keyValue = event.key;
+    if (
+      !isNaN(keyValue) &&
+      parseInt(keyValue) >= 0 &&
+      parseInt(keyValue) <= 9
+    ) {
+      if (displayField.textContent === "0") {
+        displayField.textContent = keyValue;
+      } else {
+        displayField.textContent += keyValue;
+      }
+    }
+  });
+}
+
+function listenForDelete() {
+  document.addEventListener("keydown", function (event) {
+    const keyValue = event.key;
+    if (keyValue === "Backspace" || keyValue === "Delete") {
+      displayField.textContent = displayField.textContent.slice(0, -1);
+    }
+  });
+}
 
 function checkValue() {
   if (
-    this.id === "field-5" ||
-    this.id === "field-6" ||
-    this.id === "field-7" ||
-    this.id === "field-9" ||
-    this.id === "field-10" ||
-    this.id === "field-11" ||
-    this.id === "field-13" ||
-    this.id === "field-14" ||
-    this.id === "field-15" ||
-    this.id === "field-17"
+    this.id === "seven" ||
+    this.id === "eight" ||
+    this.id === "nine" ||
+    this.id === "four" ||
+    this.id === "five" ||
+    this.id === "six" ||
+    this.id === "one" ||
+    this.id === "two" ||
+    this.id === "three" ||
+    this.id === "zero"
   ) {
-    addDigits();
-  } else if (this.id === "field-1") {
+    const value = document.getElementById(this.id).textContent;
+    addDigits(value);
+  } else if (this.id === "erase") {
     eraseInput();
-  } else if (this.id === "field-2") {
+  } else if (this.id === "changeSign") {
     changeSign();
-  } else if (this.id === "field-3") {
-    countPercent();
-  } else if (this.id === "field-4") {
-    divideBy();
-  } else if (this.id === "field-8") {
-    multiplyBy();
-  } else if (this.id === "field-12") {
-    subtract();
-  } else if (this.id === "field-16") {
-    add();
-  } else if (this.id === "field-18") {
+  }
+  // else if (this.id === "percent") {
+  //   countPercent();
+  // }
+  else if (this.id === "floatingNum") {
     addPoint();
-  } else if (this.id === "field-19") {
+  } else if (this.id === "equals") {
     countValue();
   }
 }
 
-function addDigits() {
-  console.log("it's a number");
+function addDigits(digit) {
+  if (displayField.textContent === "0") {
+    displayField.textContent = digit;
+  } else {
+    displayField.textContent += digit;
+  }
 }
 
 function eraseInput() {
-  console.log("have to erase");
+  displayField.textContent = "";
+  values = [];
 }
 
-function multiplyBy() {
-  console.log("multiply");
-}
+document.getElementById("divide").addEventListener("click", () => {
+  if (displayField.textContent !== "") {
+    values.push(displayField.textContent);
+    displayField.textContent = "";
+    if (values[values.length - 1] !== "/") {
+      values.push("/");
+    }
+    console.log(values);
+  } else if (displayField.textContent === "") {
+    console.log("no value");
+  }
+});
 
-function divideBy() {
-  console.log("divide");
-}
+document.getElementById("multiply").addEventListener("click", () => {
+  if (displayField.textContent !== "") {
+    values.push(displayField.textContent);
+    displayField.textContent = "";
+    if (values[values.length - 1] !== "*") {
+      values.push("*");
+    }
+    console.log(values);
+  } else if (displayField.textContent === "") {
+    console.log("no value");
+  }
+});
 
-function subtract() {
-  console.log("subtract");
-}
+document.getElementById("subtract").addEventListener("click", () => {
+  if (displayField.textContent !== "") {
+    values.push(displayField.textContent);
+    displayField.textContent = "";
+    if (values[values.length - 1] !== "-") {
+      values.push("-");
+    }
+    console.log(values);
+  } else if (displayField.textContent === "") {
+    console.log("no value");
+  }
+});
 
-function add() {
-  console.log("add");
-}
+document.getElementById("add").addEventListener("click", () => {
+  if (displayField.textContent !== "") {
+    values.push(displayField.textContent);
+    displayField.textContent = "";
+    if (values[values.length - 1] !== "+") {
+      values.push("+");
+    }
+    console.log(values);
+  } else if (displayField.textContent === "") {
+    console.log("no value");
+  }
+});
+
+document.getElementById("percent").addEventListener("click", () => {
+  if (displayField.textContent !== "") {
+    values.push(displayField.textContent);
+    displayField.textContent = "";
+    if (values[values.length - 1] !== "%") {
+      values.push("%");
+    }
+    console.log(values);
+  } else if (displayField.textContent === "") {
+    console.log("no value");
+  }
+});
 
 function countValue() {
-  console.log("equals");
-}
-
-function countPercent() {
-  console.log("percentage");
+  values.push(displayField.textContent);
+  let expression = values.join("");
+  let result = new Function("return " + expression)();
+  displayField.textContent = result;
 }
 
 function changeSign() {
-  console.log("-x");
+  displayField.textContent = -displayField.textContent;
 }
 
 function addPoint() {
   console.log("add point");
 }
 
-createButtons();
-checkValue();
+function listenForEnter() {
+  document.addEventListener("keydown", function (event) {
+    const keyValue = event.key;
+    if (keyValue === "Enter") {
+      displayField.textContent = result;
+    }
+  });
+}
+
+// function highlightField() {
+//   const selectedField = document.getElementById(this.id);
+//   selectedField.classList.add("highlight");
+//   console.log("yes");
+// }
+
+// function unhighlightField() {
+//   // const selectedField = document.getElementById(this.id);
+//   this.classList.remove("highlight");
+// }
+
+listenForNumber();
+listenForDelete();
+findButtons();
+// countValues();
